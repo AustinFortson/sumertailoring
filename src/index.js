e713043094a64b01e6f042939f59a4a1356ef121
Austin <a.rossfortson@gmail.com>
2019-09-22 12:46:42 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+console.log("Created by Austin Fortson", '\n', 
+            "Portfolio: https://austinfortson.netlify.com/", '\n',
+            "LinkedIn: https://www.linkedin.com/in/austin-fortson-916b17170", '\n',
+            "GitHub Code: https://github.com/AustinFortson/sumertailoring");
 ReactDOM.render(<App />, document.getElementById('root'));
 
